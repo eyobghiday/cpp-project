5243b61e9a2f1147ab9b4a74cc44de61dd78d4cf
eyobghiday <65328335+eyobghiday@users.noreply.github.com>
2024-10-03 00:28:54 -0400

    update 3

--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+// Todo: make better
+// Bug: make lists and vectors
 int main()
 {
-    cout << "testing dev" << endl;
+    std:: vector<int> items={1,2,3};
+    cout << "This is a test drill!" << endl;
     return 0;  
-} 
+}
